aabc075ed94d58b98fe6348f62ae1673c26e7b08
anhoder <anhoder@88.com>
2021-05-13 22:41:23 +0800

    update

--- service/like_service.go
+++ service/like_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/anhoder/netease-music/util"
+	"net/http"
 )
 
 type LikeService struct {
@@ -13,16 +14,22 @@ func (service *LikeService) Like() (float64, []byte) {
 
 	options := &util.Options{
 		Crypto:  "weapi",
+		Cookies: []*http.Cookie{
+			&http.Cookie{Name: "os", Value: "pc"},
+			&http.Cookie{Name: "appver", Value: "2.7.1.198277"},
+		},
 	}
 	data := make(map[string]string)
 	data["trackId"] = service.ID
+	data["alg"] = "itembased"
+	data["time"] = "3"
 	if service.L == "" {
 		data["like"] = "true"
 	} else {
 		data["like"] = service.L
 	}
 
-	code, reBody, _ := util.CreateRequest("POST", `https://music.163.com/weapi/radio/like?alg=itembased&trackId=`+service.ID+`&time=25`, data, options)
+	code, reBody, _ := util.CreateRequest("POST", `https://music.163.com/api/radio/like`, data, options)
 
 	return code, reBody
 }
