9ca4c8a01f74864c15a903a176f053fedb32fe3f
anhoder <anhoder@88.com>
2022-12-22 16:16:47 +0800

    feat: 支持扫码登录

---
+++ service/login_qr_service_test.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLoginQRService(t *testing.T) {
+	service := &LoginQRService{}
+	code, resp, url := service.GetKey()
+	fmt.Println(code, string(resp))
+	if code != 200 {
+		t.Fatalf("code error: %f", code)
+	}
+	if url == "" {
+		t.Fatalf("url empty: %s", url)
+	}
+
+	code, resp = service.CheckQR()
+	if code != 200 {
+		t.Fatalf("code error: %f", code)
+	}
+}
