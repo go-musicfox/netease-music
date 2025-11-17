d5bd067ae98578bc2b9acead96a4fb191278e4e7
sjz <1435608435@qq.com>
2025-11-17 11:49:49 +0800

    ✅update test for yunbei_signin

--- service/yunbei_signin_service.go
+++ service/yunbei_signin_service.go
@@ -7,16 +7,13 @@ import (
 type YunbeiSigninService struct {
 }
 
-func (service *YunbeiSigninService) Signin() (float64, []byte) {
-
-	options := &util.Options{
-		Crypto: "weapi",
-	}
-	data := map[string]string{
+func (service *YunbeiSigninService) Signin() (float64, []byte, error) {
+	data := map[string]interface{}{
 		"type": "0",
 	}
-
-	code, reBody, _ := util.CreateRequest("POST", `https://music.163.com/api/point/dailyTask`, data, options)
-
-	return code, reBody
+	cookiejar := util.GetGlobalCookieJar()
+	csrfToken := util.GetCsrfToken(cookiejar)
+	api := "https://music.163.com/weapi/point/dailyTask?csrf_token=" + csrfToken
+	code, bytesData, err := util.CallWeapi(api, data)
+	return code, bytesData, err
 }
--- service/yunbei_signin_service_test.go
+++ service/yunbei_signin_service_test.go
@@ -1,15 +1,40 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"testing"
 )
 
+type SigninResult struct {
+	Code  int32  `json:"code"`
+	Msg   string `json:"msg"`
+	Point int32  `json:"point"`
+}
+
 func TestYunbeiSigninService_Signin(t *testing.T) {
-	service := &YunbeiSigninService{}
-	code, resp := service.Signin()
-	fmt.Println(code, string(resp))
-	if code != 301 {
+	login := &LoginCellphoneService{
+		Phone:    "",
+		Password: "",
+	}
+	code, content, err := login.LoginCellphone()
+	if err != nil {
+		t.Errorf("error: %s", err.Error())
+	}
+	t.Logf("code: %f, content: %s", code, content)
+	if code != 200 {
 		t.Errorf("code error: %f", code)
+		t.Errorf("content: %s", content)
+	}
+	service := &YunbeiSigninService{}
+	code, resp, err := service.Signin()
+	if err != nil {
+		t.Errorf("error: %s", err.Error())
+	}
+	fmt.Println(code, string(resp))
+	result := SigninResult{}
+	_ = json.Unmarshal(resp, &result)
+	if code != 200 || result.Point == 0{
+		t.Errorf("签到失败！code: %f, msg: %s", code, result.Msg)
 	}
 }
