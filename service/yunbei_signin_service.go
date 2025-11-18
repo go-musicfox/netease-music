8775560a91744e1eebf0aac46aa9ca18914a76b9
sjz <1435608435@qq.com>
2025-11-18 11:01:14 +0800

    🐛fix(api): params of yunbei_signin

--- service/yunbei_signin_service.go
+++ service/yunbei_signin_service.go
@@ -8,11 +8,13 @@ type YunbeiSigninService struct {
 }
 
 func (service *YunbeiSigninService) Signin() (float64, []byte, error) {
-	data := map[string]interface{}{
-		"type": "0",
-	}
 	cookiejar := util.GetGlobalCookieJar()
 	csrfToken := util.GetCsrfToken(cookiejar)
+	data := map[string]interface{}{
+		"type": "1",
+		"checkToken": "", // 这个值由易盾通过浏览器指纹动态生成
+		"csrf_token": csrfToken,
+	}
 	api := "https://music.163.com/weapi/point/dailyTask?csrf_token=" + csrfToken
 	code, bytesData, err := util.CallWeapi(api, data)
 	return code, bytesData, err
